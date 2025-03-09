d56902b188cafb5e6f05d3e82db82a45ab1259d4
vedantchawla07 <91716002+vedantchawla07@users.noreply.github.com>
2025-03-09 16:54:31 +0530

    Update SegmentTree.java

--- SegmentTree.java
+++ SegmentTree.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-
 class SegTree{
 	static int seg[];
 	static int a[];
@@ -24,6 +23,18 @@ class SegTree{
 		int right = query(2 * index + 2, mid + 1, high, l, r);
 		return Math.min(left, right);
 
+	}
+	public static void pointUpdate(int desiredIndex, int newValue, int low, int high, int currIndex){
+		if(low == high){
+			seg[currIndex] = newValue;
+			return;
+		}
+		int mid = low + (high - low)/ 2;
+		if(desiredIndex <= mid)
+			pointUpdate(desiredIndex, newValue, low , mid, 2 * currIndex + 1);
+		else pointUpdate(desiredIndex, newValue, mid + 1, high, 2 * currIndex + 2);
+
+		seg[currIndex] = Math.min(seg[2 * currIndex + 1], seg[2 * currIndex + 2]);
 	}
 
 	public static void main(String args[]){
@@ -52,12 +63,22 @@ class SegTree{
 
 		int q;
 		q = sc.nextInt();
+		int type;
+		while(q-- > 0){
+			type = sc.nextInt();
+			if(type == 1){
+				int l = sc.nextInt();
+				int r = sc.nextInt();
+				System.out.println(query(0, 0, n - 1, l, r));	
+			}else{
+				int desiredIndex = sc.nextInt();
+				int newValue = sc.nextInt();
+				pointUpdate(desiredIndex, newValue, 0, n - 1, 0);
+				a[desiredIndex] = newValue;
+			}
 
-		for(int i = 0; i < q; i++){
-			int l = sc.nextInt();
-			int r = sc.nextInt();
-			System.out.println(query(0, 0, n - 1, l, r));
 		}
+
 
 	}
 }
